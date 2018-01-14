c821c761515ac4477e31dc6d0d22d69cd231ed41
Unknown <goncharovnikita.dev@gmail.com>
2018-01-14 17:15:36 +0300

    add count unique words method

--- back/utils.go
+++ back/utils.go
@@ -11,3 +11,16 @@ func getFileExtension(filename string) string {
 	a := strings.Split(filename, ".")
 	return a[len(a)-1]
 }
+
+// get unique words map from words array
+func getUniqueWords(words []string) (result map[string]string) {
+	result = map[string]string{}
+	for _, v := range words {
+		l := strings.ToLower(v)
+		if _, exists := result[l]; !exists {
+			result[l] = l
+		}
+	}
+
+	return
+}

7a5d754b7be9ae23783ffcee35cc3d9f058f984e
Unknown <goncharovnikita.dev@gmail.com>
2018-01-14 19:32:25 +0300

    Add book info parser

--- back/reader/fb2-reader.go
+++ back/reader/fb2-reader.go
@@ -2,14 +2,17 @@
 // parsing fb2 file formats
 package reader
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // FB2Reader implements BookReader
 // form .fb2 format
 type FB2Reader struct{}
 
 // ReadBook implementation
-func (f FB2Reader) ReadBook(data []byte) (words []string, err error) {
+func (f FB2Reader) ReadBook(data []byte) (words []string, bookInfo BookInfo, err error) {
 	fmt.Printf("fb2 reader starts read %d bytes of data...\n", len(data))
 	lines := 0
 	tagOpened := false
@@ -17,6 +20,8 @@ func (f FB2Reader) ReadBook(data []byte) (words []string, err error) {
 	tags := []string{}
 	bodyStartIndex := 0
 	bodyEndIndex := 0
+	descriptionStartIndex := 0
+	descriptionEndIndex := 0
 	for i, v := range data {
 		if !tagOpened {
 			if v == byte(60) {
@@ -24,10 +29,19 @@ func (f FB2Reader) ReadBook(data []byte) (words []string, err error) {
 			}
 		} else {
 			if v == byte(62) {
-				if tagName == "body" {
+				switch tagName {
+				case "body":
 					bodyStartIndex = i + 1
-				} else if tagName == "/body" {
+					break
+				case "/body":
 					bodyEndIndex = i - 7
+					break
+				case "description":
+					descriptionStartIndex = i + 1
+					break
+				case "/description":
+					descriptionEndIndex = i - 13
+					break
 				}
 				tagOpened = false
 				tags = append(tags, tagName)
@@ -40,10 +54,64 @@ func (f FB2Reader) ReadBook(data []byte) (words []string, err error) {
 			lines++
 		}
 	}
-	words, err = f.readBody(data[bodyStartIndex:bodyEndIndex])
+	if words, err = f.readBody(data[bodyStartIndex:bodyEndIndex]); err != nil {
+		return
+	}
+
+	if bookInfo, err = f.parseBookInfo(data[descriptionStartIndex:descriptionEndIndex]); err != nil {
+		return
+	}
+
+	bookInfo.Content = strings.Split(string(data[bodyStartIndex:bodyEndIndex]), "\n")
+
 	fmt.Printf("file contains %d lines, %d words\n", lines, len(words))
 	fmt.Printf("body starts at index %d, ends at index %d\n", bodyStartIndex, bodyEndIndex)
 
+	return
+}
+
+// parse book info
+func (f FB2Reader) parseBookInfo(desc []byte) (bookInfo BookInfo, err error) {
+	tagStart := false
+	tag := ""
+	genreStartIndex := 0
+	genreEndIndex := 0
+	authorStartIndex := 0
+	authorEndIndex := 0
+	for i, v := range desc {
+		if tagStart {
+			if v == byte(62) {
+				tagStart = false
+				switch tag {
+				case "genre":
+					genreStartIndex = i + 1
+					break
+				case "author":
+					authorStartIndex = i + 1
+					break
+				case "/genre":
+					genreEndIndex = i - 7
+					break
+				case "/author":
+					authorEndIndex = i - 8
+					break
+				}
+				tag = ""
+			} else {
+				tag += string(v)
+			}
+		} else {
+			if v == byte(60) {
+				tagStart = true
+			}
+		}
+	}
+	if authorEndIndex > 0 {
+		bookInfo.Author = string(desc[authorStartIndex:authorEndIndex])
+	}
+	if genreEndIndex > 0 {
+		bookInfo.Genre = string(desc[genreStartIndex:genreEndIndex])
+	}
 	return
 }
 
--- back/reader/interface.go
+++ back/reader/interface.go
@@ -1,6 +1,13 @@
 package reader
 
+// BookInfo type
+type BookInfo struct {
+	Author  string   `json:"author"`
+	Genre   string   `json:"genre"`
+	Content []string `json:"content"`
+}
+
 // BookReader interface
 type BookReader interface {
-	ReadBook(data []byte) ([]string, error)
+	ReadBook(data []byte) (words []string, bookInfo BookInfo, err error)
 }

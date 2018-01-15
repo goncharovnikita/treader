1be29aad331c8b8d0299f31331e2da528928a9f9
Unknown <goncharovnikita.dev@gmail.com>
2018-01-15 07:14:14 +0300

    Support CORS

--- back/server/handlers.go
+++ back/server/handlers.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 
 	"../reader"
 )
@@ -12,41 +13,49 @@ import (
 // handle new book
 func newBookHandler() http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.Method)
 		rw.Header().Add("Access-Control-Allow-Origin", "*")
-		var (
-			bookInfo reader.BookInfo
-			data     []byte
-			err      error
-			// words    []string
-			rdr reader.FB2Reader
-		)
+		rw.Header().Add("Access-Control-Allow-Headers", "content-type")
+		rw.Header().Add("content-type", "application/octet-stream")
+		start := time.Now()
+		if r.Method == http.MethodOptions {
+			rw.Header().Add("Access-Control-Allow-Method", "POST")
+			rw.WriteHeader(http.StatusNoContent)
+			fmt.Printf("%s %s %d %s\n", r.URL, r.Method, 204, time.Since(start))
+		} else if r.Method == http.MethodPost {
+			var (
+				bookInfo reader.BookInfo
+				data     []byte
+				err      error
+				// words    []string
+				rdr reader.FB2Reader
+			)
 
-		defer r.Body.Close()
+			defer r.Body.Close()
 
-		if err = r.ParseForm(); err != nil {
-			log.Println(err)
-			rw.WriteHeader(500)
+			if data, err = ioutil.ReadAll(r.Body); err != nil {
+				log.Println(err)
+				rw.WriteHeader(500)
+				fmt.Printf("%s %s %d %s\n", r.URL, r.Method, 500, time.Since(start))
+				return
+			}
+
+			if _, bookInfo, err = rdr.ReadBook(data); err != nil {
+				log.Println(err)
+				rw.WriteHeader(500)
+				fmt.Printf("%s %s %d %s\n", r.URL, r.Method, 500, time.Since(start))
+				return
+			}
+
+			fmt.Println(bookInfo.Author)
+			fmt.Println(bookInfo.Genre)
+
+			rw.WriteHeader(http.StatusNoContent)
+			fmt.Printf("%s %s %d %s\n", r.URL, r.Method, 204, time.Since(start))
 			return
+		} else {
+			rw.WriteHeader(http.StatusMethodNotAllowed)
+			fmt.Printf("%s %s %d %s\n", r.URL, r.Method, 405, time.Since(start))
 		}
-
-		fmt.Printf("%+v\n", r)
-
-		if data, err = ioutil.ReadAll(r.Body); err != nil {
-			log.Println(err)
-			rw.WriteHeader(500)
-			return
-		}
-
-		if _, bookInfo, err = rdr.ReadBook(data); err != nil {
-			log.Println(err)
-			rw.WriteHeader(500)
-			return
-		}
-
-		fmt.Println(bookInfo.Author)
-		fmt.Println(bookInfo.Genre)
-
-		rw.WriteHeader(http.StatusNoContent)
-		return
 	})
 }

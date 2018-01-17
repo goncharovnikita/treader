1c8d58bb9d05f9ee2f80dd27d4a69bd9c79634b8
Unknown <goncharovnikita.dev@gmail.com>
2018-01-17 22:37:39 +0300

    Add translate handler

--- back/server/handlers.go
+++ back/server/handlers.go
@@ -9,11 +9,17 @@ import (
 	"time"
 
 	"../reader"
+	"../translate"
 )
 
 // new book response type
 type newBookResponse struct {
 	Book reader.BookInfo `json:"book"`
+}
+
+// translate word response type
+type translateWordResponse struct {
+	Result string `json:"result"`
 }
 
 // handle new book
@@ -67,5 +73,48 @@ func newBookHandler() http.Handler {
 			rw.WriteHeader(http.StatusMethodNotAllowed)
 			fmt.Printf("%s %s %d %s\n", r.URL, r.Method, 405, time.Since(start))
 		}
+	})
+}
+
+// translate book handler
+func translateWordHandler() http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Add("Access-Control-Allow-Origin", "*")
+		start := time.Now()
+		var (
+			err        error
+			translator translate.ENRUTranslator
+			result     string
+			response   []byte
+		)
+
+		query := r.URL.Query().Get("query")
+
+		fmt.Printf("%+v\n", r)
+
+		if len(query) < 1 {
+			rw.WriteHeader(http.StatusNoContent)
+			fmt.Printf("%s %s %d %s\n", r.URL, r.Method, 204, time.Since(start))
+			return
+		}
+
+		if result, err = translator.Translate(query); err != nil {
+			log.Println(err)
+			rw.WriteHeader(http.StatusInternalServerError)
+			fmt.Printf("%s %s %d %s\n", r.URL, r.Method, 500, time.Since(start))
+			return
+		}
+
+		if response, err = json.Marshal(translateWordResponse{Result: result}); err != nil {
+			log.Println(err)
+			rw.WriteHeader(http.StatusInternalServerError)
+			fmt.Printf("%s %s %d %s\n", r.URL, r.Method, 500, time.Since(start))
+			return
+		}
+
+		rw.Write(response)
+		fmt.Printf("%s %s %d %s\n", r.URL, r.Method, 200, time.Since(start))
+
+		return
 	})
 }

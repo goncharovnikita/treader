7f21b64bc33cec2c5f3c649ec9708ff1fd025652
Unknown <goncharovnikita.dev@gmail.com>
2018-01-27 10:27:30 +0300

    Add beta build

--- back/build-docker.sh
+++ back/build-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker build --rm -f Dockerfile -t goncharovnikita/treader:back . && \
+docker build --rm -f Dockerfile --build-arg stage=$STAGE -t goncharovnikita/treader:back-$STAGE . && \
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD" && \
-docker push goncharovnikita/treader:back
+docker push goncharovnikita/treader:back-$STAGE
--- front/build-docker.sh
+++ front/build-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker build --rm -f Dockerfile -t goncharovnikita/treader:front . && \
+docker build --rm -f Dockerfile --build-arg stage=$STAGE -t goncharovnikita/treader:front-$STAGE . && \
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD" && \
-docker push goncharovnikita/treader:front
+docker push goncharovnikita/treader:front-$STAGE
